943db7648fe4853788129567d3065e27c67e6a4c
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-09 18:05:53 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,6 @@
-puts "Hello World!" #will output 'hello world' 
+puts "hello".upcase
+puts "hello".downcase
+puts "hello".reverse
+puts "hello".swapcase
+puts "hello".capitalize 
+puts "Greetings people".length
